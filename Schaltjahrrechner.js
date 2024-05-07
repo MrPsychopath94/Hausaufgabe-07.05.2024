a5e8bc47508b785845e366fe7099ff05511e07b2
MarcEhrlich <165007370+MarcEhrlich@users.noreply.github.com>
2024-05-07 15:48:34 +0200

    Schaltjahrrechner!

---
+++ Schaltjahrrechner.js
@@ -0,0 +1,44 @@
+function pruefeSchaltjahr(jahr) {
+  if (jahr % 4 !== 0) {
+    return false;
+  }
+
+  if (jahr % 100 === 0) {
+    if (jahr % 400 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function aktualisiereErgebnis(jahr, istSchaltjahr) {
+  const ergebnisbereich = document.getElementById("ergebnisbereich");
+  ergebnisbereich.innerHTML = `Das Jahr ${jahr} ist ${
+    istSchaltjahr ? "ein Schaltjahr" : "kein Schaltjahr"
+  }`;
+}
+
+document.getElementById("jahresEingabe").addEventListener("input", function () {
+  const jahrEingabe = document.getElementById("jahresEingabe");
+  const jahr = parseInt(jahrEingabe.value);
+
+  if (isNaN(jahr)) {
+    aktualisiereErgebnis(jahr, false);
+    return;
+  }
+
+  const istSchaltjahr = pruefeSchaltjahr(jahr);
+  aktualisiereErgebnis(jahr, istSchaltjahr);
+});
+let button3 = document.getElementById("bild");
+
+button3.onclick = function () {
+    if (bildElement.style.display === "none") {
+      bildElement.style.display = "block";
+    } else {
+      bildElement.style.display = "none";
+    }
+  };
